d133b44903b4baed71b6701bbaeb23f39482358e
piyush9214 <piyush.pawar@joshsoftware.com>
2025-08-23 01:10:32 +0530

    added secrete key

--- jevents-api/app/services/jwt_service.rb
+++ jevents-api/app/services/jwt_service.rb
@@ -1,5 +1,5 @@
 class JwtService
-  SECRET_KEY = Rails.application.credentials.secret_key_base
+  SECRET_KEY = ENV["SECRET_KEY"]
 
   def self.encode(payload, exp = 24.hours.from_now)
     payload[:exp] = exp.to_i
