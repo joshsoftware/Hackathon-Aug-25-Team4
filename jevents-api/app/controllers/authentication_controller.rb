b38c85c574f097ec7aaf1dd99278e9145d0a90ee
piyush9214 <piyush.pawar@joshsoftware.com>
2025-08-23 00:38:51 +0530

    remove debug

--- jevents-api/app/controllers/authentication_controller.rb
+++ jevents-api/app/controllers/authentication_controller.rb
@@ -4,7 +4,6 @@ class AuthenticationController < ApplicationController
   # POST /auth/login
   def login
     user = User.find_by(email: params[:email])
-    debugger
     if user&.authenticate(params[:password])
       token = JwtService.encode(user_id: user.id)
       render json: {
